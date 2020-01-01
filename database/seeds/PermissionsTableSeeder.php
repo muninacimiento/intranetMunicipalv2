e4eb8838cecadfdb45f439d41183fa80230e3aed
jfuentealba <juan.fuentealba@gmail.com>
2020-01-01 11:35:13 -0300

    Update Seeder User/Permissions

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -27,7 +27,7 @@ class PermissionsTableSeeder extends Seeder
     		'name' 			=> 'Navegamos por TODOS los Usuarios del Sistema',
     		'slug' 			=> 'users.index',
     		'description' 	=> 'Permiso que nos lista y muestra las acciones que podemos realizar con los Usuarios de la Intranet Municipal',
-            'user_id'       =>  1,
+            //'user_id'       =>  1,
 
     	]);
 
@@ -36,7 +36,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Muestra el Detalle de un Usuario de la Intranet Municipal',
             'slug'          => 'users.show',
             'description'   => 'Permiso que autoriza Visualizar en detalle todos los datos almacenados del Usuario seleccionado',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -45,7 +45,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Editar Usuario de la Intranet Municipal',
             'slug'          => 'users.edit',
             'description'   => 'Permiso que autoriza la Modificación los datos del Usuario seleccionado',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -54,7 +54,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Eliminar Usuario de la Intranet Municipal',
             'slug'          => 'users.destroy',
             'description'   => 'Permiso que autoriza la eliminación del Usuario seleccionado',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
         
         ]);
 
@@ -66,7 +66,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Navegamos por TODAS las Dependencias Municipales',
             'slug'          => 'dependencies.index',
             'description'   => 'Permiso que nos lista y muestra las acciones que podemos realizar con las Dependencias Municipales',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -75,7 +75,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Creación de las Dependencias Municipales',
             'slug'          => 'dependencies.create',
             'description'   => 'Permiso que autoriza la creación de nuevas Dependencias Municipales',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -84,7 +84,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Muestra el Detalle de la Dependencia Municipal',
             'slug'          => 'dependencies.show',
             'description'   => 'Permiso que autoriza Visualizar en detalle todos los datos almacenados de la Dependencia Municipal',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -93,7 +93,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Editar las Dependencias Municipales',
             'slug'          => 'dependencies.edit',
             'description'   => 'Permiso que autoriza la Modificación los datos de la Dependencia Municipal seleccionada',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -102,7 +102,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Eliminar las Dependencias Municipales',
             'slug'          => 'dependencies.destroy',
             'description'   => 'Permiso que autoriza la eliminación de la Dependencia Municipal seleccionada',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
         
         ]);
 
@@ -114,7 +114,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Navegamos por TODOS los Permisos del Sistema',
             'slug'          => 'permissions.index',
             'description'   => 'Permiso que nos lista y muestra las acciones que podemos realizar con los Permisos de la Intranet Municipal y cada unos de los sistemas presentes en esta plataforma',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -123,7 +123,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Creación de Permisos para la Intranet Municipal',
             'slug'          => 'permissions.create',
             'description'   => 'Permiso que autoriza la creación de nuevos permisos',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -132,7 +132,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Muestra el Detalle del Permiso de la Intranet Municipal',
             'slug'          => 'permissions.show',
             'description'   => 'Permiso que autoriza Visualizar en detalle todos los datos almacenados del Permiso seleccionado',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -141,7 +141,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Editar Permiso de la Intranet Municipal',
             'slug'          => 'permissions.edit',
             'description'   => 'Permiso que autoriza la Modificación los datos del Permiso seleccionado',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -150,7 +150,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Eliminar Permiso de la Intranet Municipal',
             'slug'          => 'permissions.destroy',
             'description'   => 'Permiso que autoriza la eliminación del Permiso seleccionado',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
         
         ]);
 
@@ -162,7 +162,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Navegamos por TODOS los Roles del Sistema',
             'slug'          => 'roles.index',
             'description'   => 'Permiso que nos lista y muestra las acciones que podemos realizar con los Roles de la Intranet Municipal',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -171,7 +171,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Creación de Roles para la Intranet Municipal',
             'slug'          => 'roles.create',
             'description'   => 'Permiso que autoriza la creación de nuevos Roles del sistema',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -180,7 +180,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Mustra el Detalle el Rol de la Intranet Municipal',
             'slug'          => 'roles.show',
             'description'   => 'Permiso que autoriza Visualizar en detalle todos los datos almacenados del Rol seleccionado',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -189,7 +189,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Editar Rol de la Intranet Municipal',
             'slug'          => 'roles.edit',
             'description'   => 'Permiso que autoriza la Modificación los datos del Rol seleccionado',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -198,7 +198,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Eliminar Rol de la Intranet Municipal',
             'slug'          => 'roles.destroy',
             'description'   => 'Permiso que autoriza la eliminación del Rol seleccionado',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
         
         ]);
 
@@ -211,7 +211,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Administración de las Solicitudes Generales',
             'slug'          => 'administration.solicitudGeneral.index',
             'description'   => 'Permiso que da acceso al listado de las Solicitudes Generales.',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -220,7 +220,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Muestra el Detalle de la Solicitud General',
             'slug'          => 'administration.solicitudGeneral.show',
             'description'   => 'Permiso que autoriza la visualización más completa de la Solicitud General',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -229,7 +229,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Editar Solicitud General',
             'slug'          => 'administration.solicitudGeneral.edit',
             'description'   => 'Permiso que autoriza la modificación de la Solicitud General seleccionada',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -238,7 +238,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Anular una Solicitud General',
             'slug'          => 'administration.solicitudGeneral.destroy',
             'description'   => 'Permiso que autoriza la anulación de la Solicitud General seleccionada',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -247,7 +247,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Administración de las Solicitudes de Actividades',
             'slug'          => 'administration.solicitudActividad.index',
             'description'   => 'Permiso que da acceso al listado de las Solicitudes de Actividades.',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -256,7 +256,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Muestra el Detalle de la Solicitud de Actividad',
             'slug'          => 'administration.solicitudActividad.show',
             'description'   => 'Permiso que autoriza la visualización más completa de la Solicitud Actividad',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -265,7 +265,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Editar Solicitud de Actividad',
             'slug'          => 'administration.solicitudActividad.edit',
             'description'   => 'Permiso que autoriza la modificación de la Solicitud de Actividad seleccionada',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -274,7 +274,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Anular una Solicitud de Actividad',
             'slug'          => 'administration.solicitudActividad.destroy',
             'description'   => 'Permiso que autoriza la anulación de la Solicitud de Actividad seleccionada',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -286,7 +286,7 @@ class PermissionsTableSeeder extends Seeder
     		'name' 			=> 'Acceso al Sistema de Gestión y Control de Proyectos Municipales - GesPRO ',
     		'slug' 			=> 'gespro.index',
     		'description' 	=> 'Permiso que nos autoriza a utilizar el Sistema de Gestión y Control de Proyectos Municipales - GesPRO',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
     	]);
 
@@ -298,7 +298,7 @@ class PermissionsTableSeeder extends Seeder
     		'name' 			=> 'Acceso al Sistema de Farmacia Municipal',
     		'slug' 			=> 'farmacia.index',
     		'description' 	=> 'Permiso que nos autoriza a utilizar el Sistema de Farmacia Municipal',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
     	]);
 
@@ -310,7 +310,7 @@ class PermissionsTableSeeder extends Seeder
     		'name' 			=> 'Acceso al Sistema de Compras Públicas Municipales - SisCoM',
     		'slug' 			=> 'siscom.index',
     		'description' 	=> 'Permiso que nos autoriza a utilizar el Sistema de Compras Públicas Municipales - SisCoM',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
     	]);
 
@@ -319,7 +319,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Acceso a las Solicitudes Generales',
             'slug'          => 'siscom.solicitudGeneral.index',
             'description'   => 'Permiso que da acceso al listado de las Solicitudes Generales.',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -328,7 +328,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Muestra el Detalle de la Solicitud General',
             'slug'          => 'siscom.solicitudGeneral.show',
             'description'   => 'Permiso que autoriza la visualización más completa de la Solicitud General',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -337,7 +337,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Editar Solicitud General',
             'slug'          => 'siscom.solicitudGeneral.edit',
             'description'   => 'Permiso que autoriza la modificación de la Solicitud General seleccionada',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -346,7 +346,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Anular una Solicitud General',
             'slug'          => 'siscom.solicitudGeneral.destroy',
             'description'   => 'Permiso que autoriza la anulación de la Solicitud General seleccionada',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -355,7 +355,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Acceso a las Solicitudes de Actividades',
             'slug'          => 'siscom.solicitudActividad.index',
             'description'   => 'Permiso que da acceso al listado de las Solicitudes de Actividades.',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -364,7 +364,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Muestra el Detalle de la Solicitud de Actividad',
             'slug'          => 'siscom.solicitudActividad.show',
             'description'   => 'Permiso que autoriza la visualización más completa de la Solicitud Actividad',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -373,7 +373,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Editar Solicitud de Actividad',
             'slug'          => 'siscom.solicitudActividad.edit',
             'description'   => 'Permiso que autoriza la modificación de la Solicitud de Actividad seleccionada',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
@@ -382,7 +382,7 @@ class PermissionsTableSeeder extends Seeder
             'name'          => 'Anular una Solicitud de Actividad',
             'slug'          => 'siscom.solicitudActividad.destroy',
             'description'   => 'Permiso que autoriza la anulación de la Solicitud de Actividad seleccionada',
-            'user_id'      =>  1,
+            //'user_id'      =>  1,
 
         ]);
 
