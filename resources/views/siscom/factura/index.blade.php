886f1b9ad8d4b47c935587ea7190182dee4779fb
Juan Fuentealba <jfuentealba@localhost.localdomain>
2020-11-05 16:26:51 -0300

    Updated November 5

--- resources/views/siscom/factura/index.blade.php
+++ resources/views/siscom/factura/index.blade.php
@@ -809,6 +809,14 @@
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
+    <!-- Boostrap Select -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min.js"></script>
+    <!-- (Optional) Latest compiled and minified JavaScript translation files -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/i18n/defaults-*.min.js"></script>
+
 <script type="text/javascript">
         
         $(document).ready(function () {
